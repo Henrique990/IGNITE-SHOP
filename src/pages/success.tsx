e885d28ab56cf44f4f58b813b96bf8e7520ae77e
Henrique Teixeira <henriqueteixeira26@gmail.com>
2023-01-17 22:12:55 -0300

    uupdate - Product & Checkout - Working with SEO

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,6 @@
 import { GetServerSideProps } from "next"
 import Image from "next/image"
+import Head from "next/head"
 import Link from "next/link"
 import Stripe from "stripe"
 import { stripe } from "../lib/stripe"
@@ -12,23 +13,28 @@ interface SuccessProps {
         imageUrl: string
     }
 }
-export default function Success({ costumerName, product}: SuccessProps) {
+export default function Success({ costumerName, product }: SuccessProps) {
     return (
-        <SuccessContainer>
-            <h1>Compra efetuada</h1>
+        <>
+            <Head>
+                <title>Compra efetuada | Ignite Shop</title>
+                <meta name="robots" content="noindex" />
+            </Head>
+            <SuccessContainer>
+                <h1>Compranefetuada</h1>
+                <ImageContainer>
+                    <Image src={product.imageUrl} width={120} height={110} alt="" />
+                </ImageContainer>
 
-            <ImageContainer>
-                <Image src={product.imageUrl} width={120} height={110} alt="" />
-            </ImageContainer>
+                <p>
+                    Uhuul <strong>{costumerName}</strong>, sua <strong>{product.name}</strong> já está a caminho da sua casa.
+                </p>
 
-            <p>
-                Uhuul <strong>{costumerName}</strong>, sua <strong>{product.name}</strong> já está a caminho da sua casa. 
-            </p>
-
-            <Link href="/">
-                Voltar ao catálogo
-            </Link>
-        </SuccessContainer>
+                <Link href="/">
+                    Voltar ao catálogo
+                </Link>
+            </SuccessContainer>
+        </>
     )
 }
 
@@ -41,23 +47,23 @@ export const getServerSideProps: GetServerSideProps = async ({ query }) => {
             }
         }
     }
-    
+
     const sessionId = String(query.session_id);
-  
+
     const session = await stripe.checkout.sessions.retrieve(sessionId, {
-      expand: ['line_items', 'line_items.data.price.product']
+        expand: ['line_items', 'line_items.data.price.product']
     });
-  
+
     const costumerName = session.customer_details.name;
     const product = session.line_items.data[0].price.product as Stripe.Product;
-  
+
     return {
-      props: {
-        costumerName,
-        product: {
-          name: product.name,
-          imageUrl: product.images[0]
+        props: {
+            costumerName,
+            product: {
+                name: product.name,
+                imageUrl: product.images[0]
+            }
         }
-      }
     }
-  }
+}
