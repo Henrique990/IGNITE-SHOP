5f13f0825c81022991d1f8a47a203fe8cfe9578a
Henrique Teixeira <henriqueteixeira26@gmail.com>
2023-01-20 21:47:28 -0300

    attempt to deploy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -35,24 +35,24 @@ export default function Home({ products }: HomeProps) {
       <Head>
         <title>Home | Ignite Shop</title>
       </Head>
-      <HomeContainer ref={sliderRef} className="keen-slider">
-        {products.map(product => {
-          return (
-            <Link href={`/product/${product.id}`} key={product.id} prefetch={false}>
-              <Product className="keen-slider__slide">
-                <Image src={product.imageUrl} width={520} height={480} alt="" />
+  <HomeContainer ref={sliderRef} className="keen-slider">
+    {products.map(product => {
+      return (
+        <Link href={`/product/${product.id}`} key={product.id} prefetch={false}>
+          <Product className="keen-slider__slide">
+            <Image src={product.imageUrl} width={520} height={480} alt="" />
 
-                <footer>
-                  <strong>{product.name}</strong>
-                  <span>{product.price}</span>
-                </footer>
-              </Product>
-            </Link>
-          )
-        })}
-      </HomeContainer>
-    </>
-  )
+            <footer>
+              <strong>{product.name}</strong>
+              <span>{product.price}</span>
+            </footer>
+          </Product>
+        </Link>
+      )
+    })}
+  </HomeContainer>
+</>
+    )
 }
 
 export const getStaticProps: GetStaticProps = async () => {
